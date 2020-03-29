e2f7340d55c46442caae2899d0f8a5ab44153e1b
ProGramer964 <CaiqueCarvalho9@hotmail.com>
2020-03-29 17:31:14 -0300

    alteração

--- server.js
+++ server.js
@@ -1,75 +1,104 @@
-
-const express = require("express")
+// Usei o express para criar e configurar meu servidor 
+const express = require('express') //Importa o modulo "express" do arquivo package.json
 const server = express()
 
-const db = require("./db")
+const db= require("./db.js")
 
-server.use(express.static("/"))
 
-server.use(express.urlencoded({ extended: true}))
+// Configurar arquivos estáticos  express (css,scripts,imagens)
+server.use(express.static("public"))
 
+// Habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }))
+
+// Configuração do nunjucks
 const nunjucks = require("nunjucks")
-nunjucks.configure("/", {
-    express: server,
-    noCache: true, // boolean
+nunjucks.configure("views", {
+    express: server, //Linka com express através da variavel server
+    noCache: true, //boolean
 })
-server.get("/", function (req, res) {
 
-    db.all(`SELECT * FROM ideas`, function(err, rows){
-        if (err){
-            console.log(err)
-         return res.send("Erro no banco de dados!")
+
+
+// Criei uma rota /
+// E capturo o pedido do cliente para responder
+server.get("/", function(req, res){
+
+    db.all(`SELECT * FROM ideas`, function(err,rows){ //Seleciona todos os registros de "ideas" //Captura erros e rows(linhas)
+    if (err){
+        console.log(err)
+        return res.send("Erro no banco de dados!")
+    } 
+
+    const reversedIdeas = [...rows].reverse() // Todo o conteudo de "rows" é revertido e jogado para a variavel reversedIdeas
+
+    let lastIdeas = []   //Let permite que o valor da variavel seja modificado
+    for (let idea of reversedIdeas){
+        if(lastIdeas.length < 2){
+            lastIdeas.push(idea)
         }
+    }
 
-        const reversedIdeas = [...rows].reverse()
+    return res.render("index.html", { ideas: lastIdeas })
+}) 
 
-        let lastIdeas = []
-        for (let idea of reversedIdeas) {
-            if (lastIdeas.length < 2) {
-                lastIdeas.push(idea)
-            }
-        }
-        return res.render("index.html", { ideas: lastIdeas })
-    })
 })
-server.get("ideias", function (req, res) {
+
+
+
+server.get("/ideias", function(req, res){
+
     
 
-
-    db.all(`SELECT * FROM ideas`, function(err, rows){
+    db.all(`SELECT * FROM ideas`, function(err,rows){
         if (err){
             console.log(err)
-         return res.send("Erro no banco de dados!")
-        }    
+            return res.send("Erro no banco de dados!")
+        } 
+
         const reversedIdeas = [...rows].reverse()
-        return res.render("ideias.html", { ideas: reversedIdeas })
+
+        return res.render("ideias.html",{ideas : reversedIdeas})
     })
 })
 
-server.post("/", function(req, res){
+server.post("/",function(req,res){
+    // Inserir dado na tabela
     const query = `
+
     INSERT INTO ideas(
         image,
         title,
         category,
         description,
         link
-    ) VALUES(?,?,?,?,?);
-    `
+
+    ) VALUES(?,?,?,?,?);     
+ `
+
     const values = [
         req.body.image,
         req.body.title,
         req.body.category,
         req.body.description,
-        req.body.link,
-    ]
-    db.run(query, values, function (err) {
-        if (err){
-            console.log(err)
-         return res.send("Erro no banco de dados!")
-        }
-        return res.redirect("ideias")
-    })
+        req.body.link
+ ]
+
+ db.run(query, values, function(err){
+    if (err){
+        console.log(err)
+        return res.send("Erro no banco de dados!")
+    } 
+
+    return res.redirect("/ideias")
+     
+  }) 
+    
+
 })
 
+// liguei meu servidor na porta 3000
 server.listen(3000)
+
+
+
