1498297d944b21d9d7ffc1b53bbb7e3f18955173
ProGramer964 <CaiqueCarvalho9@hotmail.com>
2020-03-29 17:34:37 -0300

    alteração

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-function onOff(){
+function onOff() {
     document
         .querySelector("#modal")
         .classList
@@ -15,7 +15,7 @@ function onOff(){
         .toggle("addScroll")
 }
 
-function checkFields(event){
+function checkFields(event) {
     const valuesToCheck = [
         "title",
         "image",
@@ -23,20 +23,19 @@ function checkFields(event){
         "description",
         "link",
     ]
-    const isEmpty = valuesToCheck.find(function(value){
-        
+    const isEmpty = valuesToCheck.find(function (value) {
+
         const checkIfIsString = typeof event.target[value].value === "string"
         const checkIfIsEmpty = !event.target[value].value.trim()
 
-        if(checkIfIsString && checkIfIsEmpty ) {
+        if (checkIfIsString && checkIfIsEmpty) {
             return true
 
         }
     })
-    if(isEmpty){
+    if (isEmpty) {
         event.preventDefault()
         alert("Por favor, Preencha todos os campos")
     }
 }
-        
-        
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express') //Importa o modulo "express" do arquivo package.json
 const server = express()
 
-const db= require("./db.js")
+const db = require("./db.js")
 
 
 // Configurar arquivos estáticos  express (css,scripts,imagens)
@@ -22,47 +22,47 @@ nunjucks.configure("views", {
 
 // Criei uma rota /
 // E capturo o pedido do cliente para responder
-server.get("/", function(req, res){
+server.get("/", function (req, res) {
 
-    db.all(`SELECT * FROM ideas`, function(err,rows){ //Seleciona todos os registros de "ideas" //Captura erros e rows(linhas)
-    if (err){
-        console.log(err)
-        return res.send("Erro no banco de dados!")
-    } 
-
-    const reversedIdeas = [...rows].reverse() // Todo o conteudo de "rows" é revertido e jogado para a variavel reversedIdeas
-
-    let lastIdeas = []   //Let permite que o valor da variavel seja modificado
-    for (let idea of reversedIdeas){
-        if(lastIdeas.length < 2){
-            lastIdeas.push(idea)
+    db.all(`SELECT * FROM ideas`, function (err, rows) { //Seleciona todos os registros de "ideas" //Captura erros e rows(linhas)
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
         }
-    }
 
-    return res.render("index.html", { ideas: lastIdeas })
-}) 
+        const reversedIdeas = [...rows].reverse() // Todo o conteudo de "rows" é revertido e jogado para a variavel reversedIdeas
+
+        let lastIdeas = []   //Let permite que o valor da variavel seja modificado
+        for (let idea of reversedIdeas) {
+            if (lastIdeas.length < 2) {
+                lastIdeas.push(idea)
+            }
+        }
+
+        return res.render("index.html", { ideas: lastIdeas })
+    })
 
 })
 
 
 
-server.get("/ideias", function(req, res){
+server.get("/ideias", function (req, res) {
 
-    
 
-    db.all(`SELECT * FROM ideas`, function(err,rows){
-        if (err){
+
+    db.all(`SELECT * FROM ideas`, function (err, rows) {
+        if (err) {
             console.log(err)
             return res.send("Erro no banco de dados!")
-        } 
+        }
 
         const reversedIdeas = [...rows].reverse()
 
-        return res.render("ideias.html",{ideas : reversedIdeas})
+        return res.render("ideias.html", { ideas: reversedIdeas })
     })
 })
 
-server.post("/",function(req,res){
+server.post("/", function (req, res) {
     // Inserir dado na tabela
     const query = `
 
@@ -82,18 +82,18 @@ server.post("/",function(req,res){
         req.body.category,
         req.body.description,
         req.body.link
- ]
+    ]
 
- db.run(query, values, function(err){
-    if (err){
-        console.log(err)
-        return res.send("Erro no banco de dados!")
-    } 
+    db.run(query, values, function (err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
 
-    return res.redirect("/ideias")
-     
-  }) 
-    
+        return res.redirect("/ideias")
+
+    })
+
 
 })
 
